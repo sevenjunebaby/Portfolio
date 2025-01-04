4a803623034972473fa2e014a8d7106e0ad919b9
sevenjune <yahiawissal76@gmail.com>
2025-01-04 22:05:13 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,47 +3,22 @@ import React from "react";
 const Contact = () => {
   return (
     <section id="contact" className="py-6">
-      <h2 className="text-3xl font-bold mb-4">Contact</h2>
-      <form className="max-w-md">
-        <div className="mb-4">
-          <label className="block mb-2 font-bold" htmlFor="name">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="w-full border rounded p-2"
-            placeholder="Your Name"
-          />
+      <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
+      <div className="links">
+      <div className="cnt">
+        <img src="github.png" alt="github" className="cnt-img"></img>
         </div>
-        <div className="mb-4">
-          <label className="block mb-2 font-bold" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="w-full border rounded p-2"
-            placeholder="Your Email"
-          />
+        <div className="cnt"> 
+        <img src="linkedin.jpg" alt="linkedin" className="cnt-img" ></img>
+        
         </div>
-        <div className="mb-4">
-          <label className="block mb-2 font-bold" htmlFor="message">
-            Message
-          </label>
-          <textarea
-            id="message"
-            className="w-full border rounded p-2"
-            placeholder="Your Message"
-          />
+        <div className="cnt">
+        <img src="youtube.png" alt="youtube" className="cnt-img"></img>
         </div>
-        <button
-          type="submit"
-          className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
-        >
-          Send
-        </button>
-      </form>
+       
+
+      </div>
+     
     </section>
   );
 };
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,21 +8,7 @@ const Portfolio = () => {
 
   return (
     <section id="portfolio" className="py-6">
-      <h2 className="text-3xl font-bold mb-4">Portfolio</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {projects.map((project, index) => (
-          <div
-            key={index}
-            className="border p-4 rounded-lg shadow hover:shadow-lg transition"
-          >
-            <h3 className="text-xl font-bold">{project.name}</h3>
-            <p>{project.description}</p>
-            <a href={project.link} className="text-blue-600 hover:underline">
-              View Project
-            </a>
-          </div>
-        ))}
-      </div>
+    
     </section>
   );
 };
