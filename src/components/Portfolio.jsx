10ac705c2e670a17f4f44985461c3107b199cfeb
sevenjune <yahiawissal76@gmail.com>
2025-01-06 21:45:28 +0100

    update portfolio part logos

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,7 +12,13 @@ const Portfolio = () => {
 
     Web Development: Technologies like HTML, CSS, JavaScript, React, Node.js, and Laravel will help you build interactive websites and web applications.
     Mobile Development: You can use frameworks like React Native, Flutter, or Java for Android development to create mobile apps.
-    </p> </div>
+    </p>
+    
+    <img src="webandmobilelogo.png" alt="logo1" className="logo" />
+
+    
+    
+     </div>
        <div className="box">
        <img src="ai.png" alt="aiandml" />
    <p>
@@ -22,7 +28,11 @@ const Portfolio = () => {
     Programming languages like Python and libraries such as TensorFlow, PyTorch, and scikit-learn are essential.
     Learn about data preprocessing, neural networks, and AI concepts like NLP (Natural Language Processing) and computer vision.
     Tools like LangChain and your experience with chatbots are a great foundation for AI-driven applications.
-    </p>  </div>
+    </p> 
+    
+    <img src="aiandmllogo.png" alt="logo2" className="logo" />
+    
+     </div>
        <div className="box">
        <img src="cyber.png" alt="cyber" />
        <p>
@@ -31,7 +41,10 @@ const Portfolio = () => {
 Explore concepts like network security, encryption, penetration testing, and ethical hacking.
 Tools like Kali Linux, Wireshark, and Burp Suite can help you learn practical security techniques.
 Certifications like CEH (Certified Ethical Hacker) or CISSP (Certified Information Systems Security Professional) can boost your career in this domain.
-</p> </div>
+</p> 
+<img src="cyberlogo.png" alt="logo3" className="logo" />
+
+</div>
 
     </section>
   );
