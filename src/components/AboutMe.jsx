c7e548c0114ac39de03bab8d2221835fd9d05be7
sevenjune <yahiawissal76@gmail.com>
2025-08-30 08:50:26 +0100

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -13,7 +13,7 @@ const AboutMe = () => {
     </h1>
      
       <p>
-     Software Engineer with 4 years of experience, specializing in Generative AI and Data Science.
+     AI Engineer Student with 3 years of experience, specializing in Generative AI and Data Science.
       A versatile professional who can be described as a Full-Stack Developer or a Multidisciplinary Software Engineer. </p>
       <div className="cv">
         <a href="/cv.pdf" download>
